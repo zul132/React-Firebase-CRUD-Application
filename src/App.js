611c01c4d9dd8723eb365e99625e1bbfa4d69590
Fathima Zulaikha <fathimazulaikha.25cs@licet.ac.in>
2023-12-15 15:11:46 +0530

    Implemented CRUD operations using Firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import { db } from './firebase-config';
+import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc } from 'firebase/firestore';
 
 function App() {
+  const [newName, setNewName] = useState("");
+  const [newAge, setNewAge] = useState(0);
+  // create a state variable to hold the list of users
+  const [users, setUsers] = useState([]);
+  // create a reference to our firestore collection
+  const usersCollectionRef = collection(db, 'users');
+
+  //function to create a new user
+  const createUser = async () => {
+    /* addDoc() is used to add a new doc to our Firestore collection */
+    await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
+  };
+
+  //function to update the user's age by increasing it by 1
+  const updateUser = async (id, age) => {
+    const userDoc = doc(db, "users", id); /* <-- this fetches the document that we want to update */
+    const newFields = { age: age + 1 };
+
+    /*updateDoc() will update the specified fields for the given document */
+    await updateDoc(userDoc, newFields);
+  };
+
+  //function to delete a user
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "users", id);
+    await deleteDoc(userDoc);
+  };
+
+  useEffect(() => {
+    const getUsers = async () => {
+      /* getDocs() will return all the documents inside a specified collection */
+      const data = await getDocs(usersCollectionRef);
+
+      // Set the users state to the array of users in our collection
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+  }, []); // <-- Use an empty dependency array to run the effect only once
+
   return (
     <div className="App">
-      
+      <input type='text' placeholder='Enter Name' onChange={ (event) => {setNewName(event.target.value); }} /><br />
+      <input type='number' placeholder='Enter Age' onChange={ (event) => {setNewAge(event.target.value); }} /><br /><br />
+      <button onClick={createUser}>Create User</button>
+      <hr />
+
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+            <button onClick={ () => { updateUser(user.id, user.age); } }>Increment Age</button>&nbsp;&nbsp;
+            <button onClick={ () => { deleteUser(user.id)} }>Delete User</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from '@firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC9z5HAOoEk7_QxtdqrgLFCvIeXOLkEjk8",
-    authDomain: "react-firebase-aa809.firebaseapp.com",
-    projectId: "react-firebase-aa809",
-    storageBucket: "react-firebase-aa809.appspot.com",
-    messagingSenderId: "281394989837",
-    appId: "1:281394989837:web:cf732c7d52b0f818e03e44",
-    measurementId: "G-YT4QL6X56Q"
-  };
+  apiKey: "AIzaSyC9z5HAOoEk7_QxtdqrgLFCvIeXOLkEjk8",
+  authDomain: "react-firebase-aa809.firebaseapp.com",
+  projectId: "react-firebase-aa809",
+  storageBucket: "react-firebase-aa809.appspot.com",
+  messagingSenderId: "281394989837",
+  appId: "1:281394989837:web:cf732c7d52b0f818e03e44",
+  measurementId: "G-YT4QL6X56Q"
+};
 
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
   
-  export const db = getFirestore(app);
+export const db = getFirestore(app);
